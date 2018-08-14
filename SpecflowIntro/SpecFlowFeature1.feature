cf04b959f9bf37d777a4ea7b89df0bd69387d8a9
PreetySehrawat <preety_sehrawat@yahoo.com>
2018-08-14 12:48:14 +1200

    My first commit

---
+++ SpecflowIntro/SpecFlowFeature1.feature
@@ -0,0 +1,48 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@smoketest
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	And I press subtract
+	Then the result should be 121 on the screen
+	And I view result
+
+Scenario: Create a new employee with mandatory details
+#Given I opened my application
+#Then I should see employee details page
+When I fill all the mandatory details in form
+| Name   | Age | Phone      | Email        |
+| Myname | 25  | 98765432331 | abcd@xyz.com |
+#And I click on Save button
+#Then I should see all the details saved in my application and DB
+
+Scenario: Create four new employee with mandatory details
+#Given I opened my application
+#Then I should see employee details page
+When I fill all the mandatory details in form for four
+| Name   | Age | Phone      | Email        |
+| Myname | 25  | 98765432331 | abcd@xyz.com |
+| SecondName | 26  | 987554432331 | abcd2@xyz.com |
+| ThirdName | 27  | 987554432771 | abcd3@xyz.com |
+| FourthName | 28  | 987554432881 | abcd4@xyz.com |
+#And I click on Save button
+#Then I should see all the details saved in my application and DB
+
+
+Scenario Outline: Create four new employee using examples
+#Given I opened my application
+#Then I should see employee details page
+When I fill all the mandatory details in form <Name>, <Age> , <Phone> and <Email>
+#And I click on Save button
+#Then I should see all the details saved in my application and DB
+Examples:
+| Name   | Age | Phone      | Email        |
+| Myname | 25  | 98765432331 | abcd@xyz.com |
+| SecondName | 26  | 987554432331 | abcd2@xyz.com |
+| ThirdName | 27  | 987554432771 | abcd3@xyz.com |
+| FourthName | 28  | 987554432881 | abcd4@xyz.com |
---
+++ SpecflowIntro/SpecFlowFeature1Steps.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecflowIntro
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int numbers)
+        {
+            Console.WriteLine(numbers);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Pressed Add Button");
+        }
+
+        [When(@"I press subtract")]
+        public void WhenIPressSubtract()
+        {
+            Console.WriteLine("Pressed Subtract Button");
+        }
+
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 121)
+                Console.WriteLine("Test Passed");
+            else
+            { 
+                Console.WriteLine("Test Failed");
+                throw new Exception("The Value is different");
+            }
+
+
+        }
+        [Then(@"I view result")]
+        public void ThenIViewResult()
+        {
+            Console.WriteLine("Viewed Result");
+        }
+
+        [When(@"I fill all the mandatory details in form")]
+        public void WhenIFillAllTheMandatoryDetailsInForm(Table table)
+        {
+           Employeedetails details = table.CreateInstance<Employeedetails>();
+            Console.WriteLine(details.Name);
+            Console.WriteLine(details.Age);
+            Console.WriteLine(details.Phone);
+            Console.WriteLine(details.Email);
+        }
+
+        [When(@"I fill all the mandatory details in form for four")]
+        public void WhenIFillAllTheMandatoryDetailsInFormForFour(Table table)
+        {
+            var details = table.CreateSet<Employeedetails>();
+            foreach (Employeedetails emp in details)
+            {
+                Console.WriteLine("Details of Employee" + emp.Name);
+                Console.WriteLine("________________________________________");
+                Console.WriteLine(emp.Name);
+                Console.WriteLine(emp.Age);
+                Console.WriteLine(emp.Phone);
+                Console.WriteLine(emp.Email);
+            }
+        }
+
+
+        [When(@"I fill all the mandatory details in form (.*), (.*) , (.*) and (.*)")]
+        public void WhenIFillAllTheMandatoryDetailsInFormMynameAndAbcdXyz_Com(string name,int age, Int64 phone, string email)
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Phone:" + phone);
+            Console.WriteLine("Email:" + email);
+        }
+
+
+    }
+}
